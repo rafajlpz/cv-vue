5c25e796fb564a59794d656a801a38062dd19bec
rafajlpz <frilo_94@hotmail.com>
2022-05-10 14:45:05 +0200

    Añadido y actualizado css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <MiMenu/>
   </nav>
   <router-view/>
   <mis-datos></mis-datos>
 </template>
 
-<style lang="scss">
+<style>
 
 </style>
 <script setup>
+import MiMenu from './components/MiMenu.vue';
 
 </script>
 
--- src/components/MiMenu.vue
+++ src/components/MiMenu.vue
@@ -1,15 +1,41 @@
 <template>
   <nav class="main-menu">
-      
+    <router-link to="/">Sobre mi</router-link>
+
+    |
+
+    <router-link to="/about">Contacto</router-link>
+
+    |
+
+    <router-link to="/about">Random</router-link>
+
+    |
   </nav>
 </template>
 
-<script>
-export default {
-
-}
-</script>
+<script></script>
 
 <style>
-
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.main-menu {
+  border: 1px solid black;
+  background-color: aqua;
+  display: flex;
+}
+.main-menu a {
+  display: block;
+  text-decoration: none;
+  padding: .5em;
+  color: white;
+  
+}
+.main-menu a:hover{
+  background-color: #0a4f7f;
+  border-color:#0a4f7f ;
+}
 </style>
