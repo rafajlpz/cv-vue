3e82ca0a19562eb1eac74dcf7632feb2574216fb
rafajlz <frilo_94@hotmail.com>
2022-05-11 20:02:27 +0200

    Agregado cajas flex y ajustado anchos y altura

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,65 @@
 <template>
   <div class="main-home">
-    <div class="imagen-container"><img src="../assets/alpaquita.jpg" alt=""></div>
-    <div class="contenedor-texto1"></div>
-    <div class="contenedor-texto2"></div>
-
+    <div class="imagen-container">
+      <img src="../assets/alpaquita.jpg" alt="" />
+      <h3>Rafael Idrogo Lopez</h3>
+      <h2>Me presento:</h2>
+      <p class="descripcion">
+        Intento de programador junior con conocimientos de html5, css, js y vue.
+        Amante de los juegos online siendo antiguo jugador 'profesional' de
+        e-sports en Counter-Strike, ahora me dedico a picar codigo
+      </p>
+    </div>
+    <hr />
+    <div class="contenedor-texto1">
+      <h2>Experiencia laboral</h2>
+      <p>sfasfasdfasdfSDFHGSDFGSDasdfasfasd</p>
+    </div>
+    <hr>
+    <div class="contenedor-texto2">
+      <h2>Titulos</h2>
+      <p>SDFGKLSDJKLGJASFJASFJKOASFHASJKFAS</p>
+    </div>
   </div>
 </template>
 
 <script>
-
-
-
 </script>
 
 <style>
-.main-home{
+.main-home {
   margin: auto;
   width: 62.5rem;
   height: 100%;
   background-color: rgba(174, 194, 194, 0.4);
 }
 
-
-.imagen-container{
+.imagen-container {
   display: flex;
-  /* justify-content: center; */
-  padding: .75em;
+  margin: auto;
+  width: 95%;  
+  padding: 0.75em;
 }
-.imagen-container img{
+.imagen-container img {
+  border: 2px solid black;
+  border-radius: 10px;
+  height: 10rem;
+  width: 8rem;
+}
+.imagen-container h3 {
+}
+.contenedor-texto1{
+  display: flex;
+  margin: auto;
+  width: 95%;
+  padding: 0.75em;
+}
+
+.contenedor-texto2{
+  display: flex;
   
+  margin: auto;
+  width: 95%;
+  padding: 0.75em;
 }
 </style>
