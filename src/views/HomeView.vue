7de75c1101e3e328ecff48d3117e816a6dcd3d80
rafajlpz <frilo_94@hotmail.com>
2022-05-12 08:39:30 +0200

    Actualizado css, añadido div para esctructurar mejor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,40 @@
 <template>
   <div class="main-home">
     <div class="imagen-container">
-      <img src="../assets/alpaquita.jpg" alt="" />
-      <h3>Rafael Idrogo Lopez</h3>
-      <h2>Me presento:</h2>
-      <p class="descripcion">
-        Intento de programador junior con conocimientos de html5, css, js y vue.
-        Amante de los juegos online siendo antiguo jugador 'profesional' de
-        e-sports en Counter-Strike, ahora me dedico a picar codigo
-      </p>
+      
+        <img src="../assets/alpaquita.jpg" alt="" />
+    
+      <div class="imagen-description">
+        <h2>Me presento</h2>
+        <p>
+          Intento de programador junior con conocimientos de html5, css, js y
+          vue. Amante de los juegos online siendo antiguo jugador 'profesional'
+          de e-sports en Counter-Strike, ahora me dedico a picar codigo
+        </p>
+      </div>
     </div>
+
     <hr />
+
     <div class="contenedor-texto1">
-      <h2>Experiencia laboral</h2>
-      <p>sfasfasdfasdfSDFHGSDFGSDasdfasfasd</p>
+      <div class="description-texto1">
+        <h2>Experiencia laboral</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ullam labore amet, voluptates sapiente quam soluta fugit quia suscipit blanditiis maxime pariatur, in, architecto itaque qui doloremque praesentium aut fugiat esse placeat! Consequatur officiis molestiae quaerat illum qui officia ea doloremque, ab quia ipsa velit amet maxime! Magnam consequatur, pariatur esse, autem voluptatem illum odio molestiae sit assumenda non fugiat sunt at nostrum? Sequi provident veniam illo rerum sint deleniti accusamus numquam aliquam natus consequuntur similique ducimus dolores, totam nostrum doloribus tempora. Id debitis, amet blanditiis harum saepe minima placeat maiores obcaecati nulla perspiciatis dolorem maxime officiis autem minus molestias dolores culpa possimus repellat voluptas eos eveniet vel accusamus explicabo voluptatibus! Iste obcaecati consequuntur eos rem nam sequi impedit magnam illum praesentium quas exercitationem ut ducimus voluptas nobis neque expedita pariatur quibusdam laudantium, mollitia quia quos possimus ipsa! Pariatur unde velit non molestias laborum esse nostrum molestiae delectus, assumenda libero, quo voluptates ut iusto incidunt perspiciatis ipsa placeat qui vero reiciendis rem soluta labore repellendus maxime. Repellendus corrupti libero molestiae aperiam accusamus quaerat suscipit rem non reprehenderit sapiente ipsam in dignissimos, voluptatibus, voluptas unde vitae deleniti tempore error excepturi. Voluptas quisquam at accusantium fugit officiis, quae totam cum dignissimos tenetur eius molestias esse, odit officia iste cumque laborum veritatis tempore velit quis earum illo. Porro veniam architecto dignissimos sed quo iusto natus? Eius aliquam cum est illum sit, sed pariatur, in aliquid nam obcaecati voluptatum, maiores architecto ut aperiam ex! Enim eveniet nisi provident harum. Ipsam excepturi voluptates eligendi deserunt provident consequuntur ex inventore et optio quas in, omnis quae officiis ab iure vel. Maiores, velit et voluptas cumque nemo corrupti soluta tempora ipsam alias eaque dignissimos nihil obcaecati totam ut inventore natus ad rem? Qui aperiam aspernatur cupiditate provident error blanditiis dolorem voluptatem? Vero accusamus beatae dignissimos autem at.</p>
+      </div>
     </div>
-    <hr>
+
+    <hr />
+
     <div class="contenedor-texto2">
-      <h2>Titulos</h2>
-      <p>SDFGKLSDJKLGJASFJASFJKOASFHASJKFAS</p>
+      <div class="description-texto2">
+        <h2>Titulos</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo deserunt perspiciatis accusamus mollitia expedita animi error officiis impedit ea aliquam esse consequuntur incidunt, recusandae eaque tempora culpa? Perspiciatis, minus odio? Doloremque, repellat? Iste natus possimus eius libero quis nam vero, rerum cumque perspiciatis amet, alias ipsum quisquam tenetur voluptas sed omnis. Laborum amet saepe optio voluptatibus tenetur, possimus in, dolores dicta, rerum facilis nisi at. Enim, consectetur at tempore hic alias vero ipsa cupiditate aspernatur mollitia, dolorem quam veritatis ducimus nostrum fugit ut aperiam id suscipit tenetur eveniet rerum velit quod pariatur. Dolor illum ipsum neque blanditiis doloremque placeat, soluta temporibus totam dignissimos quod libero voluptatem quia nostrum amet! Voluptatibus, aperiam! Atque voluptatum eaque corrupti, est ipsa at recusandae illo! Eligendi iusto voluptates fugit! Tempora quas ducimus nobis? Explicabo commodi sunt totam quod modi eum itaque et, libero quam accusamus reiciendis placeat, praesentium eveniet voluptas laborum qui, repudiandae sapiente. Eum in fugit dolore, omnis reprehenderit illo perspiciatis est tempore sapiente! Ducimus vel corrupti minima tempora animi quaerat sapiente ullam molestiae quam officia. Numquam doloremque eum nobis nesciunt inventore, iusto animi. Architecto tenetur atque deleniti repudiandae distinctio, ipsum perspiciatis quis quidem voluptatem quia facilis porro vero, saepe explicabo aut qui cumque.</p>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
 .main-home {
@@ -37,7 +47,7 @@
 .imagen-container {
   display: flex;
   margin: auto;
-  width: 95%;  
+  width: 95%;
   padding: 0.75em;
 }
 .imagen-container img {
@@ -48,16 +58,16 @@
 }
 .imagen-container h3 {
 }
-.contenedor-texto1{
+.contenedor-texto1 {
   display: flex;
   margin: auto;
   width: 95%;
   padding: 0.75em;
 }
 
-.contenedor-texto2{
+.contenedor-texto2 {
   display: flex;
-  
+
   margin: auto;
   width: 95%;
   padding: 0.75em;
