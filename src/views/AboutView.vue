685f6a30d630cd23f1231cdb378d84fefcbfbc7d
rafajlpz <frilo_94@hotmail.com>
2022-06-01 12:09:49 +0200

    Añadido background y cambios varios
    - Agregado formulario
    - Cambios css
    - Aumento de fuente

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,20 +2,19 @@
   <div class="contenedor">
     <div class="caja-formulario">
       <h1>Pagina de acceso</h1>
-      <br>
-      <hr>
-      <br>
+      <br />
+      <hr />
+      <br />
       <form @submit.prevent="autentificar" action="#" id="myForm">
         <div class="control">
           <label for="email"> Correo electronico </label>
           <input type="email" id="email" />
         </div>
-    
         <div class="control">
           <label for="password"> Contraseña </label>
           <input type="password" id="password" />
         </div>
-        <br>
+        <br />
         <button>Log In</button>
       </form>
     </div>
@@ -23,32 +22,27 @@
 </template>
 
 <script setup>
-const autentificar = () =>{
-  alert("preufFG");
-}
-
+const autentificar = () => {
+  alert("probando...");
+};
 </script>
 
 <style>
-
-.contenedor{
+.contenedor {
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
-.caja-formulario{
+.caja-formulario {
   background-color: grey;
   border: 2px solid black;
-  
+
   width: 300px;
   height: fit-content;
 
   display: flex;
   flex-direction: column;
   justify-content: center;
-  
-  
-
 }
 </style>
